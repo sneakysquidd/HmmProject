5391ec3d279f5528d08d9af04d35066e1e9ccf8b
_gavbob <gsct2002@gmail.com>
2018-12-04 12:34:23 -0700

    Added View controllers with different colors and labels and images

---
+++ HmmProject/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HmmProject
+//
+//  Created by Thomas, Gavin on 12/4/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
